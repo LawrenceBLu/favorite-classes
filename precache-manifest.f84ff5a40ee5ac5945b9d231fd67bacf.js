53ec56e02a372efb429358fa0742ae854dbaba18
Lawrence Lu <reach_lawrence@yahoo.com>
2022-02-11 01:32:03 -0800

    Updates

---
+++ precache-manifest.f84ff5a40ee5ac5945b9d231fd67bacf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e2a9ad6f6d06896e03b500e40f3ff6",
+    "url": "/favorite-classes/index.html"
+  },
+  {
+    "revision": "bb15539248bb7f5b8e2d",
+    "url": "/favorite-classes/static/css/main.b9b4eab3.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/favorite-classes/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/favorite-classes/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "bb15539248bb7f5b8e2d",
+    "url": "/favorite-classes/static/js/main.046912b3.chunk.js"
+  },
+  {
+    "revision": "6ec7a0a62b752538ad7a",
+    "url": "/favorite-classes/static/js/runtime~main.b72faa2a.js"
+  }
+]);
